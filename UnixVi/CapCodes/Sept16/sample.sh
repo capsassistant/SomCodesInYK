aa722d45909dcb534bcdb5bf3c0d8bc4c62bc980
capsassistant <90681835+capsassistant@users.noreply.github.com>
2021-09-17 23:07:01 +0530

    Created sample.sh and its Question

---
+++ UnixVi/CapCodes/Sept16/sample.sh
@@ -0,0 +1,13 @@
+echo "file in $1 are "
+ls -l $1
+
+
+
+
+#8. Write a shell script that takes a directory as command line argument and displays the files and sub-directories in that directory.
+<<inputs
+with input example as 
+
+sh sample.sh /home/tr1159773
+
+inputs
