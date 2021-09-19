b916d4fd01187103f83d05285d5159cb63a466fb
capsassistant <90681835+capsassistant@users.noreply.github.com>
2021-09-19 19:28:47 +0530

    Created Point.java file, first in folder
    first file for java11

---
+++ Java11/Essentials/Exercises/Point.java
@@ -0,0 +1,40 @@
+class Point {
+	
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	//Method to obtain distance of a point w.r.t origin
+	public double distance(){
+	    //code here
+	}
+	
+	//Method to obtain distance of a point w.r.t another point
+	public double distance(Point point){
+	    //code here
+	}
+
+}
+class Tester {
+	public static void main(String[] args) {
+	    //code here
+    		
+	}
+}
