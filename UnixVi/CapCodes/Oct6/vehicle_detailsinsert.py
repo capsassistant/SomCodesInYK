cab725eb138e95e2cde96d6de2d6ecbc10501ba4
capsassistant <90681835+capsassistant@users.noreply.github.com>
2021-10-06 04:10:00 +0530

    added the insert format commands

--- UnixVi/CapCodes/Oct6/vehicle_detailsinsert.py
+++ UnixVi/CapCodes/Oct6/vehicle_detailsinsert.py
@@ -28,3 +28,16 @@ US31D463 	1008 	Kia Rio 	Hatchback 	4 	29.00
 US31D464 	1009 	Chevrolet Bolt 	Mini 	3 	17.00
 US31D465 	1010 	Honda Pilot 	SUV 	6 	69.00
 """
+
+"""
+INSERT INTO vehicle_details VALUES('US31D456',1001,'Toyota Corolla','Sedan',4,'39.00');
+INSERT INTO vehicle_details VALUES('US31D457',1002,'Subaru Crosstek','SUV',6,'49.00');
+INSERT INTO vehicle_details VALUES('US31D458',1003,'Toyota Highlander','SUV',6,'59.00');
+INSERT INTO vehicle_details VALUES('US31D459',1004,'Honda Fit','Mini',3,'19.00');
+INSERT INTO vehicle_details VALUES('US31D460',1005,'Volkswagon GTI','Hatchback',5,'27.00');
+INSERT INTO vehicle_details VALUES('US31D461',1006,'Honda Civic','Sedan',4,'34.00');
+INSERT INTO vehicle_details VALUES('US31D462',1007,'Kia Optima','Sedan',4,'29.00');
+INSERT INTO vehicle_details VALUES('US31D463',1008,'Kia Rio','Hatchback',4,'29.00');
+INSERT INTO vehicle_details VALUES('US31D464',1009,'Chevrolet Bolt','Mini',3,'17.00');
+INSERT INTO vehicle_details VALUES('US31D465',1010,'Honda Pilot','SUV',6,'69.00');
+"""
