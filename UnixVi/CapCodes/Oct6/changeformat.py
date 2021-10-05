49d6d9e615a2908e282e2c3a1c9d09b6c2b24f54
capsassistant <90681835+capsassistant@users.noreply.github.com>
2021-10-06 01:22:22 +0530

    Update changeformat.py

--- UnixVi/CapCodes/Oct6/changeformat.py
+++ UnixVi/CapCodes/Oct6/changeformat.py
@@ -8,7 +8,7 @@ with open(datas) as f:
 for data in f:
 data = data.encode("ascii", "ignore").decode()
 data=re.sub('\s+',' ',data)
-pattern=r"^([\w]*[\d]*) current_date [-– ]* ([\d]*) days ([\w ]*) (\d*)$"
+pattern=r"^([\w]*[\d]*) current_date [-–. ]* ([\d]*) days ([\w ]*) (\d*)$"
 insertis=r"INSERT INTO coupon_details VALUES('\1',date_sub(curdate(),interval \2 day),'\3',\4),"
 values.append(re.sub(pattern,insertis, data))
 
